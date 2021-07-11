c32cec86a819944c2540559e23a4e64f140c8722
tincho.tr <fmtroncoso.prog@gmail.com>
2021-07-11 03:40:56 -0300

    biloba

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.2 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Función de encogimiento de la barra de navegación
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Reducir la barra de navegación
+    navbarShrink();
+
+    // Reducir la barra de navegación cuando se desplaza la página
+    document.addEventListener('scroll', navbarShrink);
+
+    // Active Bootstrap scrollspy en el elemento de navegación principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Contraer la barra de navegación receptiva cuando el conmutador está visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
